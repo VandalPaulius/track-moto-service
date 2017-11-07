57c57cd509a6db67ff0321fa79e4149f66c1f22b
DaniusKalv <danius.kalvaitis@gmail.com>
2017-11-07 22:09:32 +0000

    Mongodb connection
    Added mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
 
 app.use(bodyParser.json());
 
