9720fc12a2848b900f4d19e584653d14ddd6f8fc
DaniusKalv <danius.kalvaitis@gmail.com>
2017-11-08 15:12:20 +0000

    Post request is now being handled
    The server handles post requst and inserts the json data to mongoDB

--- index.js
+++ index.js
@@ -3,20 +3,36 @@ const bodyParser = require('body-parser');
 const app = express();
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-
 var url = 'mongodb://localhost:27017/test';
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
-});
+
+var insertTrackingData = function(db, device_id, timestamp, latitude, longitude, callback) {
+  db.collection('tracking_data').insertOne({
+    "device_id": device_id,
+    "timestamp": timestamp,
+    "latitude": latitude,
+    "longitude": longitude
+  }, function(err, result) {
+   assert.equal(err, null);
+   console.log("Inserted a document into the tracking_data collection.");
+   callback();
+ });
+};
 
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Here goes website'));
 app.post('/api/user/location', (req, res) => {
-    console.log("requestbody", req.body);
+    console.log("requestbody", JSON.stringify(req.body.device_id));
     res.status(200).send('OK');
+
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      insertTrackingData(db, req.body.device_id, req.body.timestamp, 
+        req.body.latitude, req.body.longitude, function() {
+          db.close();
+      });
+    });
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
