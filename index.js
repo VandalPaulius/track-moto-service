98ba45d85e0e5066850fd8604abde41266ac27dc
DaniusKalv <danius.kalvaitis@gmail.com>
2017-12-05 17:30:39 +0000

    Implemented basic user authentication and authorization

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const db_collection = 'tracking_data';
+
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var mongo_url = 'mongodb://localhost:27017/test';
 
-var createUser = function(db, email, password, username, callback){
+var routes = require('./routes/router');
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+//Connect to MongoDB
+mongoose.connect(mongo_url);
+var db = mongoose.connection;
+
+//Handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
+
+//Use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+//Parse incoming requests
+app.use(bodyParser.json());
+
+//Include routes
+
+app.use('/', routes);
+
+/*var createUser = function(db, email, password, username, callback){
   db.collection('users').insertOne({
     "email": email,
     "password": password,
@@ -16,6 +50,15 @@ var createUser = function(db, email, password, username, callback){
     console.log("Created user");
     callback();
   });
+}*/
+
+/*var checkUserEmail = function(email, callback){
+  var test;
+  MongoClient.connect(mongo_url, function (err, db) {
+    test = db.collection('users').find({email: email}, {email: 0, password: 0, username: 0}).limit(1);
+    db.close();
+  });
+  return test;
 }
 
 var createDevice = function(db, user_id, phone_number, device_name){
@@ -54,44 +97,21 @@ var insertTrackingData = function(db, device_id, timestamp, latitude, longitude,
    console.log("Inserted tracking data");
    callback();
  });
-};
+};*/
 
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => res.send('Here goes website'));
-
-app.post('/api/user', (req, res) =>{ //Create user
-  console.log("Creating user ", JSON.stringify(req.body.username));
-  res.status(200).send('OK');
-
-  MongoClient.connect(mongo_url, function(err, db){
-    assert.equal(null, err);
-    createUser(db, req.body.email, req.body.password, req.body.username, 
-    function() {
-      db.close();
-    });
-  });  
-}); 
-
-app.post('/api/user/login')
-app.post('/api/:uid/trackers') //create trackers
-app.get('/api/:uid/trackers') //get trackers
-
-app.post('/api/:uid/trackers/:tracker_uid/location', (req, res) => {
-    console.log("device_id", JSON.stringify(req.body.device_id));
-    res.status(200).send('OK');
-
-  MongoClient.connect(mongo_url, function(err, db) {
-    assert.equal(null, err);
-    insertTrackingData(db, req.body.device_id, req.body.timestamp, 
-      req.body.latitude, req.body.longitude, 
-      function() {
-        db.close();
-    });
-  });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('File Not Found');
+  err.status = 404;
+  next(err);
 });
 
-app.get('/api/:uid/trackers/:tracker_uid/location')
+// error handler
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
---
+++ routes/router.js
@@ -0,0 +1,109 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+
+function requiresLogin(req, res, next) {
+  if (req.session && req.session.userId) {
+    return next();
+  } else {
+    var err = new Error('You must be logged in to view this page.');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+// GET route after registering
+router.get('/', requiresLogin, (req, res, next) => {
+  User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        if (user === null) {
+          var err = new Error('Not authorized! Go back!');
+          err.status = 400;
+          return next(err);
+        } else {
+          return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
+        }
+      }
+    });
+});
+
+//Create user
+router.post('/api/user', (req, res) =>{ 
+  console.log("Creating user ", JSON.stringify(req.body.username));
+  res.status(200).send('OK');
+
+  var userData = {
+    email: req.body.email,
+    password: req.body.password,
+    username: req.body.username
+  }
+
+  User.create(userData, function(error, user){
+    if(error){
+
+    } else{
+      req.session.userId = user._id;
+      return res.redirect('/');
+    }
+  });  
+}); 
+
+//Check if specified email exists
+router.get('/api/user/checkEmail/:email', (req, res) => {
+  MongoClient.connect(mongo_url, function (err, db) {
+    db.collection('users').find({email: email}, {email: 0, password: 0, username: 0}).limit(1);
+    db.close();
+  });
+  checkUserEmail(req.params.email);
+});
+
+router.post('/api/user/login', (req, res) =>{ 
+  User.authenticate(req.body.email, req.body.password, function (error, user) {
+    if (error || !user) {
+      var err = new Error('Wrong email or password.');
+      err.status = 401;
+      return next(err);
+    } else {
+      req.session.userId = user._id;
+      return res.redirect('/');
+    }
+  });
+});
+
+//GET for logout
+router.get('/api/user/logout', (req, res, next) => {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+router.post('/api/:uid/trackers') //create trackers
+router.get('/api/:uid/trackers') //get trackers
+
+router.post('/api/:uid/trackers/:tracker_uid/location', (req, res) => {
+    console.log("device_id", JSON.stringify(req.body.device_id));
+    res.status(200).send('OK');
+
+  MongoClient.connect(mongo_url, function(err, db) {
+    assert.equal(null, err);
+    insertTrackingData(db, req.body.device_id, req.body.timestamp, 
+      req.body.latitude, req.body.longitude, 
+      function() {
+        db.close();
+    });
+  });
+});
+
+router.get('/api/:uid/trackers/:tracker_uid/location')
+
+module.exports = router;
