cf1f16fdc35a34c380d5c42be397578b8a62d89d
Paulius Juska <paulius.juska@gnatta.com>
2017-12-31 01:31:36 +0000

    Added login and logiuyt logic

--- routes/authorization.js
+++ routes/authorization.js
@@ -1,17 +1,35 @@
+var User = require(`${__basedir}/models/user`);
+
 const login = (db, req, res, next) => {
-  res.json({
-    lucky: 'bastard',
-    message: 'You\'ve called login'
-  })
-  res.sendStatus(200);
+  User.authenticate(req.body.email, req.body.password, (error, user) => {
+    if (error) {
+      res.status(400)
+      res.write(`Unexpected error has occured: ${error}`)
+      res.end();
+    } else if (!user) {
+      res.status(404)
+      res.write('User not found')
+      res.end();
+    } else {
+      req.session.userId = user._id;
+      
+      res.status(200)
+      res.end();
+    }
+  });
 }
 
 const logout = (db, req, res, next) => {
-  res.json({
-    lucky: 'bastard',
-    message: 'You\'ve called logout'
-  })
-  res.sendStatus(200);
+  if (req.session) {
+    // delete session object
+    req.session.destroy((err) => {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
 }
 
 exports.configure = (router, db) => {
