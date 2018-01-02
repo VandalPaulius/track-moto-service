30a0e2ffcc564eae2a14c6cee2e3b1857e7465ca
Paulius Juska <paulius.juska@gnatta.com>
2018-01-02 15:46:18 +0000

    Changed error to appropriate code.

--- router/routes/user.js
+++ router/routes/user.js
@@ -1,24 +1,15 @@
-var validator = require('validator');
 var User = require(`${__basedir}/models/user`);
 
 const create = (db, req, res, next) => {
-  // if (!validator.isEmail(req.body.email)) {
-  //   var err = new Error('Wrong email');
-  //   err.status = 400;
-  //   return next(err);
-  // }
-
   var userData = {
     email: req.body.email,
     password: req.body.password,
     username: req.body.username
   };
 
-  console.log('req', req);
-
   User.create(userData, (error, user) => {
     if (error) {
-      res.status(401)
+      res.status(400)
       res.write(`Unexpected error has occured: ${error}`)
       res.end();
     } else {
