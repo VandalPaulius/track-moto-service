dfdba0e696a0ce26f3698d1eab95e4a4bd8a01f7
Paulius Juska <paulius.juska@gnatta.com>
2018-01-22 15:44:09 +0000

    Refactored User model to improve readability.

--- models/user.js
+++ models/user.js
@@ -6,7 +6,6 @@ var UserSchema = new mongoose.Schema({
     type: String,
     unique: true,
     required: true,
-    trim: true
   },
   password:{
     type: String,
@@ -14,38 +13,45 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
-//Authenticate input against database
-UserSchema.statics.authenticate = function (email, password, callback) {
-  User.findOne({ email: email })
-    .exec(function (err, user) {
-      if (err) {
-        return callback(err)
-      } else if (!user) {
-        var err = new Error('User not found.');
-        err.status = 401;
-        return callback(err);
-      }
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (result === true) {
-          return callback(null, user);
-        } else {
-          return callback();
-        }
-      })
-    });
-}
+// //Authenticate input against database
+// UserSchema.statics.authenticate = function (email, password, callback) {
+//   User.findOne({ email })
+//     .exec(function (err, user) {
+//       if (err) {
+//         return callback(err)
+//       } else if (!user) {
+//         var err = new Error('User not found.');
+//         err.status = 401;
+//         return callback(err);
+//       }
+//       bcrypt.compare(password, user.password, function (err, result) {
+//         if (result === true) {
+//           return callback(null, user);
+//         } else {
+//           return callback();
+//         }
+//       })
+//     });
+// }
 
-//Hashing a password before saving it to the database
+// Hashing a password before saving it to the database
 UserSchema.pre('save', function (next) {
   var user = this;
-  bcrypt.hash(user.password, 10, function (err, hash) {
+  bcrypt.genSalt(10, (err, salt) => {
     if (err) {
       return next(err);
     }
-    user.password = hash;
-    next();
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      
+      user.password = hash;
+      next(null, user);
+    })
   })
+ 
 });
 
-var User = mongoose.model('User', UserSchema);
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
